a9ffbb198e48fb46cd61663c73f691ee2908e1e2
Patrick Van Weelden <pvanweel@yahoo.com>
2020-08-17 14:57:04 -0500

    Chapter 81

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,7 @@ const Header = () => (
     <header>
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active"  exact={true}>Dashboard</NavLink>
-        <br/>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <br/>
-        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-        <br/>
         <NavLink to="/help" activeClassName="is-active">Help</NavLink>
     </header>
 );
