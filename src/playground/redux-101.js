fb98134db1140479199a09ab27697d7276e8709e
Patrick Van Weelden <pvanweel@yahoo.com>
2020-08-18 09:36:17 -0500

    Chapter 87

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,35 +3,54 @@ import {createStore} from 'redux';
 const store = createStore((state = { count: 0 },action) => {
     switch (action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             }
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             }
         case 'RESET':
             return {
                 count: state.count = 0
+            }
+        case 'SET':
+            return {
+                count: state.count = action.count
             }
         default:
             return state;
     }
 });
 
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+})
+
 store.dispatch({
-    type: "INCREMENT"
+    type: "INCREMENT",
+    incrementBy: 5
 });
 
 store.dispatch({
     type: "INCREMENT"
 });
+
 store.dispatch({
     type: "RESET"
 });
 
 store.dispatch({
-    type: "DECREMENT"
+    type: "DECREMENT",
+    decrementBy: 5
 });
 
-console.log(store.getState());
+store.dispatch({
+    type: "SET",
+    count: 101
+});
+
+
+
