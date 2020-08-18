57950c3c317d5f36c6d4e77c5a5dbc6847ba64c1
Patrick Van Weelden <pvanweel@yahoo.com>
2020-08-18 14:41:43 -0500

    Chapter 91

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -27,7 +27,11 @@ const setCount = ({count} ) => ({
 
 })
 
-const store = createStore((state = { count: 0 },action) => {
+// Reducers
+// 1) Reducers are pure functions
+// 2) Never change State or Action
+
+const countReducer = (state = { count: 0 },action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
@@ -48,7 +52,9 @@ const store = createStore((state = { count: 0 },action) => {
         default:
             return state;
     }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
---
+++ src/playground/redux-expensify.js
@@ -0,0 +1,19 @@
+import {createStore, combineReducers} from 'redux'
+
+const demoState = {
+    expenses: [{
+        id:'2345',
+        description: 'January Rent',
+        note: 'This was the final payment for that address',
+        amount: 54500,
+        createdAt: 0
+    }],
+    filters: {
+        text: 'rent',
+        sortBy: 'amount',
+        startDate: undefined,
+        endDate: undefined
+    }
+}
+
+
