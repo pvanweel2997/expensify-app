2e56cb4c8d4042cb07851b58ceb2fd1aae83af0d
Patrick Van Weelden <pvanweel@yahoo.com>
2020-08-19 08:53:36 -0500

    Chapter 93 Spread operator in reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,7 +1,32 @@
 import {createStore, combineReducers} from 'redux'
+import uuid from 'uuid'
 
 // ADD EXPENSE
+
+const addExpense = (
+    { description = '', 
+      note = '', 
+      amount = 0, 
+      createdAt = 0 
+    } = {}
+) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+})
+
 // REMOVE EXPENSE
+
+const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+})
+
 // EDIT EXPENSE
 // SET TEXT FILTER
 // SORT_BY_DATE
@@ -14,12 +39,27 @@ import {createStore, combineReducers} from 'redux'
 const expenseReducerDefaultState = [];
 
 const expenseReducer = (state = expenseReducerDefaultState, action) => {
+    if (action.type === 'REMOVE_EXPENSE') {
+        console.log('id: '+action.id)
+    }
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state, 
+                action.expense]
+        case 'REMOVE_EXPENSE':
+            return state.filter(({id}) => id !== action.id)
         default:
             return state;
     }
 }
 
+
+// handleDeleteOption = (optionToRemove) => {
+//     this.setState ((prevState) =>  ({
+//         options: prevState.options.filter((option) => optionToRemove !== option)
+//      }))
+//  };
 // Filters Reducer
 
 const filtersReducerDefaultState = {
@@ -45,7 +85,16 @@ const store = createStore(
     })
 );
 
-console.log(store.getState())
+store.subscribe(() => {
+    console.log(store.getState())
+})
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100}));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300}));
+
+store.dispatch(removeExpense({id: expenseOne.expense.id}));
+
+
 
 const demoState = {
     expenses: [{
