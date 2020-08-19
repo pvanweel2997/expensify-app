58fb15a9247566df40f918fa652247940b9739b5
Patrick Van Weelden <pvanweel@yahoo.com>
2020-08-18 19:36:09 -0500

    Chapter 92

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,52 @@
 import {createStore, combineReducers} from 'redux'
 
+// ADD EXPENSE
+// REMOVE EXPENSE
+// EDIT EXPENSE
+// SET TEXT FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// Expenses Reducer
+
+const expenseReducerDefaultState = [];
+
+const expenseReducer = (state = expenseReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+// Filters Reducer
+
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+}
+
+const filterReducer = (state = filtersReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+// store creation
+
+const store = createStore(
+    combineReducers({
+        expenses: expenseReducer,
+        filters: filterReducer
+    })
+);
+
+console.log(store.getState())
+
 const demoState = {
     expenses: [{
         id:'2345',
