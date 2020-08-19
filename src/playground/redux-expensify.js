3088ed7ed767d11a34964e4c49ca437e22760ebf
Patrick Van Weelden <pvanweel@yahoo.com>
2020-08-19 15:38:23 -0500

    Chapter 97 sorting redux data

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -143,6 +143,12 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate}) => {
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if (sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1;
+        } else if (sortBy = 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
     })
 }
 
@@ -161,15 +167,15 @@ store.subscribe(() => {
     console.log(visibleExpenses)
 })
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000}));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -21000}));
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -1000}));
 
 // store.dispatch(removeExpense({id: expenseOne.expense.id}));
 
 // store.dispatch(editExpense(expenseTwo.expense.id, {  'amount': 500}))
-store.dispatch(setTextFilter('rent'))
+// store.dispatch(setTextFilter('rent'))
 
-// store.dispatch(sortByAmount())
+store.dispatch(sortByAmount())
 // store.dispatch(sortByDate())
 
 // store.dispatch(setStateDate(0))
@@ -192,13 +198,3 @@ const demoState = {
     }
 }
 
-const user = {
-    name: 'Jen',
-    age: 24
-}
-
-console.log({
-    ...user,
-    location: 'Waukee',
-    age: 9
-});
