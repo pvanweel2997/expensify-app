2d97ea22e20b9811a21ac08ceb5ba3c2d1e4622a
Patrick Van Weelden <pvanweel@yahoo.com>
2020-08-20 11:17:29 -0500

    Chapter 101 - connecting store and components with React-Redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore';
 import {addExpense} from './actions/expenses';
@@ -16,10 +17,21 @@ const expenseTwo = store.dispatch(addExpense({ description: 'Gas', amount: 12600
 store.dispatch(setTextFilter('gas'))
 console.log(store.getState());
 
+setTimeout(() => {
+    store.dispatch(setTextFilter('water'))
+}, 5000);
+
+
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
     console.log(visibleExpenses)
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'));
 
 
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import ExpenseDashboardPage from './ExpenseDashboardPage';
+
+
+const ExpenseList = (props) => (
+    <div>
+        <h1>Expense List</h1>
+        {props.expenses.length}
+        {props.filters.text}
+    </div>
+);
+
+const mapStateToProps = (state) => {
+    return {
+        expenses: state.expenses,
+        filters: state.filters
+    }
+};
+
+export default  connect(mapStateToProps)(ExpenseList);
+
+
