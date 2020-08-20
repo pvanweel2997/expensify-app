df820e023b8146ff4c3bba652e862cdbee61cda4
Patrick Van Weelden <pvanweel@yahoo.com>
2020-08-20 13:52:32 -0500

    Chapter 103 - Controlled inputs for Filters

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -8,7 +8,6 @@ import selectExpenses from '../selectors/expenses';
 const ExpenseList = (props) => (
     <div>
         <h1>Expense List</h1>
-        {props.expenses.length}    
         {props.expenses.map((expense) => {
             return <ExpenseListItem key={expense.id}{...expense} />
         })}
