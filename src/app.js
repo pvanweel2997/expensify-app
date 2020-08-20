0d40e442d9649ae97b7661d6e25ebfe60d742431
Patrick Van Weelden <pvanweel@yahoo.com>
2020-08-20 14:40:43 -0500

    Chapter 104 - Dropdown for picking sortBy

--- src/app.js
+++ src/app.js
@@ -12,9 +12,10 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-const expenseOne = store.dispatch(addExpense({ description: 'Water', amount: 9600, createdAt: -21000}));
-const expenseTwo = store.dispatch(addExpense({ description: 'Gas', amount: 12600, createdAt: -21000}));
-store.dispatch(setTextFilter('gas'))
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500}));
+store.dispatch(addExpense({ description: 'Gas bill',  createdAt: 1000}));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500}));
+// store.dispatch(setTextFilter('gas'))
 console.log(store.getState());
 
 // setTimeout(() => {
@@ -23,8 +24,8 @@ console.log(store.getState());
 
 
 const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-    console.log(visibleExpenses)
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+//     console.log(visibleExpenses)
 
 const jsx = (
     <Provider store={store}>
