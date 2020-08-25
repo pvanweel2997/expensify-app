1f810dd018c3ef5dd65cc9764d4438d9570c878d
Patrick Van Weelden <pvanweel@yahoo.com>
2020-08-25 15:12:06 -0500

    Chapter 123 - test spies

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -58,3 +58,32 @@ test('should not set amount if invalid input',() => {
     })
     expect(wrapper.state('amount')).toBe('');
 })
+
+test('should call onsubmit prop for valid form submission',() => {
+    const onSubmitSpy = jest.fn();
+    const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy} />);
+    wrapper.find('form').simulate('submit',{
+        preventDefault : () => { }
+    });
+    expect(wrapper.state('error')).toBe('');
+    expect(onSubmitSpy).toHaveBeenLastCalledWith({
+        description: expenses[0].description,
+        note: expenses[0].note,
+        amount: expenses[0].amount,
+        createdAt: expenses[0].createdAt
+    });
+});
+
+test('should set new date on date changed', () => {
+    const now = moment();
+    const wrapper = shallow(<ExpenseForm  />);
+    wrapper.find('SingleDatePicker').prop('onDateChange')(now);
+    expect(wrapper.state('createdAt')).toBe(now);
+})
+
+test('should set calendar focused on change' , () => {
+    const focused = true;
+    const wrapper = shallow(<ExpenseForm  />);
+    wrapper.find('SingleDatePicker').prop('onFocusChange')({ focused });
+    expect(wrapper.state('calendarFocused')).toBe(focused);
+})
