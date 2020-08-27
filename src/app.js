5818757e0ed79e6c1631e382e4a80c44cc81e2de
Patrick Van Weelden <pvanweel@yahoo.com>
2020-08-27 14:57:45 -0500

    Chapter 133 - production webpack

--- src/app.js
+++ src/app.js
@@ -10,23 +10,7 @@ import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-
 const store = configureStore();
-
-// store.dispatch(addExpense({ description: 'Water bill', amount: 4500}));
-// store.dispatch(addExpense({ description: 'Gas bill',  createdAt: 1000}));
-// store.dispatch(addExpense({ description: 'Rent', amount: 109500}));
-
-console.log(store.getState());
-
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('water'))
-// }, 5000);
-
-
-const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-//     console.log(visibleExpenses)
 
 const jsx = (
     <Provider store={store}>
@@ -37,3 +21,4 @@ const jsx = (
 ReactDOM.render(jsx, document.getElementById('app'));
 
 
+    
