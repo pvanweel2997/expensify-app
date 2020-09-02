82a6d4aac8057b66fbe91e89cc058a4be52fcee7
Patrick Van Weelden <pvanweel@yahoo.com>
2020-09-02 14:53:58 -0500

    Chapter 147 - updating data

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -19,20 +19,34 @@ const database = firebase.database();
 database.ref().set({
     name: 'Patrick Van Weelden',
     age: 55,
+    stressLevel: 6,
+    job: {
+        title: 'software developer',
+        company : 'google'
+    },
     isSingle : true,
     location : {
         city : 'Waukee',
         country: 'United States'
     }
+}).then(() => {
+    console.log('data is saved');
+}).catch((e)=> {
+    console.log('this failed',e);
 });
 
 
-// database.ref().set('this is my data');
+database.ref().update({
+    stressLevel: 9,
+    'job/company' : 'Amazon',
+   'location/city' : 'Seatle'
+});
 
-database.ref('age').set(56);
-database.ref('location/city').set('Oskaloosa');
 
-database.ref('attributes').set({
-    height: 71,
-    weight: 190
-})
+// database.ref('isSingle')
+// .remove()
+// .then(() => {
+//     console.log('success removing isSingle');
+// }).catch((e) => {
+//     console.log('error removing isSingle',e)
+// })
