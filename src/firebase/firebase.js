7e90f4ebae92404b8de75eb4342baa894372a86b
Patrick Van Weelden <pvanweel@yahoo.com>
2020-09-02 15:33:21 -0500

    Chapter 148 - fetching data from firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -16,31 +16,74 @@ const config = {
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Patrick Van Weelden',
-    age: 55,
-    stressLevel: 6,
-    job: {
-        title: 'software developer',
-        company : 'google'
-    },
-    isSingle : true,
-    location : {
-        city : 'Waukee',
-        country: 'United States'
-    }
-}).then(() => {
-    console.log('data is saved');
-}).catch((e)=> {
-    console.log('this failed',e);
+const onValueChange = database.ref().on('value',(snapshot) => {
+    const val = snapshot.val();
+    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
 });
-
 
 database.ref().update({
-    stressLevel: 9,
-    'job/company' : 'Amazon',
-   'location/city' : 'Seatle'
-});
+    'job/company' : 'Amazon'
+})
+
+
+
+// subscribe change notifications
+// const onValueChange = database.ref().on('value',(snapshot) => {
+//     console.log(snapshot.val());
+//   },(e) => {
+//       console.log('error with data fetching',e);
+//   })
+
+// setTimeout(() => {
+//     database.ref('age').set(24);
+// }, 3500);
+
+// // unsubscribe notification
+// setTimeout(() => {
+//     database.ref().off(onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//     database.ref('age').set(40);
+// }, 10500);
+
+
+
+// database.ref('location/city')
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     }).catch((e) => {
+//         console.log('error fetching data',e);
+//     })
+
+
+// database.ref().set({
+//     name: 'Patrick Van Weelden',
+//     age: 55,
+//     stressLevel: 6,
+//     job: {
+//         title: 'software developer',
+//         company : 'google'
+//     },
+//     isSingle : true,
+//     location : {
+//         city : 'Waukee',
+//         country: 'United States'
+//     }
+// }).then(() => {
+//     console.log('data is saved');
+// }).catch((e)=> {
+//     console.log('this failed',e);
+// });
+
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company' : 'Amazon',
+//    'location/city' : 'Seatle'
+// });
 
 
 // database.ref('isSingle')
