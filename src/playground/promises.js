c8152cca671906f96fe293c079780164e5366102
Patrick Van Weelden <pvanweel@yahoo.com>
2020-09-04 07:34:08 -0500

    Chapter 154 - Testing async redux actions part 2

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,10 +1,10 @@
 const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-    //    resolve({
-    //        name: 'Pat',
-    //        age: 55
-    //    })
-    reject('something went wrong');
+       resolve({
+           name: 'Pat',
+           age: 55
+       })
+    // reject('something went wrong');
     }, 5000);
 })
 
@@ -12,6 +12,13 @@ console.log('before');
 
 promise.then((data) => {
     console.log('1',data);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+           resolve('this is my other promise')
+        }, 5000);
+    });
+}).then((str) => {  // this is the resolve execution for the promise above
+    console.log('does this run',str)
 }).catch((error) => {
     console.log('error',error)
 })
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -2,6 +2,7 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { startAddExpense, addExpense, editExpense, removeExpense} from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
 
@@ -50,13 +51,47 @@ test('should add expense to database and store',(done) => {
         createdAt: 1000
     }
     store.dispatch(startAddExpense(expenseData)).then(() => {
-        expect(1).toBe(1);
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...expenseData
+            }
+        })
+        // returning a promise
+        return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+
+    }).then((snapshot) => { // tresolve for the promise above (return databse.ref)
+        expect(snapshot.val()).toEqual(expenseData);
         done();
     });
 })
 
-test('should add expense with defaults to database and store',() => {
-    
+test('should add expense with defaults to database and store',(done) => {
+    const store = createMockStore({});
+    const expenseDefaults = {
+        description: '',
+        amount: 0,
+        note : '',
+        createdAt: 0
+    }
+    store.dispatch(startAddExpense({})).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...expenseDefaults
+            }
+        })
+        // returning a promise
+        return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+
+    }).then((snapshot) => { // tresolve for the promise above (return databse.ref)
+        expect(snapshot.val()).toEqual(expenseDefaults);
+        done();
+    });
 })
 
 // test('should setup the add expense action object with default values', () => {
