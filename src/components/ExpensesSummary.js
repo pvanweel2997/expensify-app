5001cd90bcf95fb79bfa79c4503130efc2e40f80
Patrick Van Weelden <pvanweel@yahoo.com>
2020-09-20 18:31:09 -0500

    Chapter 173 - Styling summary area

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
@@ -9,8 +10,14 @@ export const ExpensesSummary = ({expenseCount, expensesTotal }) => {
     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
     return (
-        <div>
-            <h1>viewing {expenseCount} {expenseWord} totaling {formattedExpensesTotal}</h1>
+        <div className="page-header">
+            <div className="content-container"> 
+                <h1 className="page-header__title">viewing <span>{expenseCount}</span> {expenseWord} totaling <span>{formattedExpensesTotal}</span></h1>
+                <div className="page-header_actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
+            
         </div>
     );
 };
